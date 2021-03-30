c3af5a1ea7d8539f2987467fefccb49aa2f9569b
Gavin <gavin.feeney01@gmail.com>
2021-03-30 20:12:25 +0100

    Add way to find all window names for #6

--- bot/bot_utils.py
+++ bot/bot_utils.py
@@ -77,3 +77,12 @@ class WindowCapture:
         img = np.ascontiguousarray(img)
 
         return img
+
+    # find the name of the window you're interested in.
+    # once you have it, update window_capture()
+    # https://stackoverflow.com/questions/55547940/how-to-get-a-list-of-the-name-of-every-open-window
+    def list_window_names(self):
+        def winEnumHandler(hwnd, ctx):
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)

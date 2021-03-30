2035003ca106a829dc6075b4c0e86e4793ac781a
Gavin <gavin.feeney01@gmail.com>
2021-03-30 20:31:17 +0100

    Add screencapture to follower_bot for #5

--- bot/follower_bot.py
+++ bot/follower_bot.py
@@ -3,6 +3,8 @@ import time
 import os
 import json
 import numpy as np
+from bot_utils import WindowCapture
+import cv2 as cv
 
 
 class FollowerBot():
@@ -17,12 +19,38 @@ class FollowerBot():
         # First detect the current player 1 and player 2 location
         self.target_pos = self.detect_other_player_loc()
         self.current_pos = self.detect_bot_loc()
+        # Change the working directory to the folder this script is in.
+        # Doing this because I'll be putting the files from each video in their own folder on GitHub
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+        # initialize the WindowCapture class
+        wincap = WindowCapture('Untitled - Notepad')
+
+        loop_time = time.time()
+        while(True):
+
+            # get an updated image of the game
+            screenshot = wincap.get_screenshot()
+
+            cv.imshow('Computer Vision', screenshot)
+
+            # debug the loop rate
+            print('FPS {}'.format(1 / (time.time() - loop_time)))
+            loop_time = time.time()
+
+            # press 'q' with the output window focused to exit.
+            # waits 1 ms every loop to process key presses
+            if cv.waitKey(1) == ord('q'):
+                cv.destroyAllWindows()
+                break
+
+        print('Done.')
 
     def detect_other_player_loc(self):
-        pass
+        return False
 
     def detect_bot_loc(self):
-        pass
+        return False
 
 
 if __name__ == "__main__":

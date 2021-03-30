d9f234e7ef7cc06f09731227a0c0dd83cccddfb3
Gavin <gavin.feeney01@gmail.com>
2021-03-30 20:11:12 +0100

    Add get screenshot for #6

--- bot/bot_utils.py
+++ bot/bot_utils.py
@@ -39,3 +39,41 @@ class WindowCapture:
         # images into actual screen positions
         self.offset_x = window_rect[0] + self.cropped_x
         self.offset_y = window_rect[1] + self.cropped_y
+
+    def get_screenshot(self):
+
+        # get the window image data
+        wDC = win32gui.GetWindowDC(self.hwnd)
+        dcObj = win32ui.CreateDCFromHandle(wDC)
+        cDC = dcObj.CreateCompatibleDC()
+        dataBitMap = win32ui.CreateBitmap()
+        dataBitMap.CreateCompatibleBitmap(dcObj, self.w, self.h)
+        cDC.SelectObject(dataBitMap)
+        cDC.BitBlt((0, 0), (self.w, self.h), dcObj,
+                   (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+
+        # convert the raw data into a format opencv can read
+        #dataBitMap.SaveBitmapFile(cDC, 'debug.bmp')
+        signedIntsArray = dataBitMap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype='uint8')
+        img.shape = (self.h, self.w, 4)
+
+        # free resources
+        dcObj.DeleteDC()
+        cDC.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, wDC)
+        win32gui.DeleteObject(dataBitMap.GetHandle())
+
+        # drop the alpha channel, or cv.matchTemplate() will throw an error like:
+        #   error: (-215:Assertion failed) (depth == CV_8U || depth == CV_32F) && type == _templ.type()
+        #   && _img.dims() <= 2 in function 'cv::matchTemplate'
+        img = img[..., :3]
+
+        # make image C_CONTIGUOUS to avoid errors that look like:
+        #   File ... in draw_rectangles
+        #   TypeError: an integer is required (got type tuple)
+        # see the discussion here:
+        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        img = np.ascontiguousarray(img)
+
+        return img

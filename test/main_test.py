730d1b92d24c292c995d05de1fa8598334a5713a
Gavin <gavin.feeney01@gmail.com>
2021-04-01 11:36:34 +0100

    Add basic detection image return illustration

--- test/main_test.py
+++ test/main_test.py
@@ -1,2 +1,21 @@
 import cv2 as cv
 import numpy as np
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def detect():
+    target_img = cv.imread(
+        'enemy.jpg', cv.IMREAD_UNCHANGED)
+    sample_img = cv.imread(
+        'capture2.jpg', cv.IMREAD_UNCHANGED)
+
+    result = cv.matchTemplate(sample_img, target_img, cv.TM_CCOEFF_NORMED)
+
+    cv.imshow("Result", result)
+    cv.waitKey()
+
+
+if __name__ == "__main__":
+    detect()

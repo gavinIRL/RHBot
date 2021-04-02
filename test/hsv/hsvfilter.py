335c3badbdf847d93327b115079a86e1c8067ecb
Gavin <gavin.feeney01@gmail.com>
2021-04-02 22:03:40 +0100

    Added known button positions for #8 and #9

--- test/hsv/hsvfilter.py
+++ test/hsv/hsvfilter.py
@@ -43,7 +43,7 @@ def grab_preset_filter(object_name=None, **kwargs) -> typing.Tuple[HsvFilter, li
     if object_name == "message_section_cleared":
         return HsvFilter(0, 0, 214, 179, 65, 255, 0, 0, 0, 17), [444, 595, 835, 700]
     if object_name == "message_go":
-        return HsvFilter(32, 114, 89, 58, 255, 255, 0, 12, 0, 0)
+        return HsvFilter(32, 114, 89, 58, 255, 255, 0, 12, 0, 0), [600, 222, 700, 275]
     if object_name == "enemy_nametag":
         return HsvFilter(49, 0, 139, 91, 30, 197, 0, 0, 40, 38), [10, 145, 1084, 684]
     if object_name == "message_boss_encounter":
@@ -54,10 +54,14 @@ def grab_preset_filter(object_name=None, **kwargs) -> typing.Tuple[HsvFilter, li
         # This is a difficult one to separate
         return HsvFilter(0, 34, 38, 28, 152, 124, 0, 0, 5, 12), [10, 145, 1084, 684]
     if object_name == "map_outline":
-        return HsvFilter(0, 85, 108, 12, 178, 205, 0, 13, 37, 50)
+        if kwargs.get("big_map"):
+            return HsvFilter(0, 128, 82, 8, 255, 255, 0, 66, 30, 34), [485, 280, 900, 734]
+        return HsvFilter(0, 128, 82, 8, 255, 255, 0, 66, 30, 34), [1100, 50, 1260, 210]
     if object_name == "gate_map_pos":
         # This is a very difficult one to separate
-        return HsvFilter(15, 45, 38, 18, 200, 135, 0, 119, 0, 0)
+        if kwargs.get("big_map"):
+            return HsvFilter(0, 128, 82, 8, 255, 255, 0, 66, 30, 34), [485, 280, 900, 734]
+        return HsvFilter(0, 128, 82, 8, 255, 255, 0, 66, 30, 34), [1100, 50, 1260, 210]
     if object_name == "prompt_move_reward_screen":
         return HsvFilter(72, 98, 92, 105, 255, 225, 0, 54, 24, 38)
     if object_name == "prompt_select_card":
@@ -74,8 +78,8 @@ def grab_preset_filter(object_name=None, **kwargs) -> typing.Tuple[HsvFilter, li
         return HsvFilter(19, 91, 107, 31, 168, 181, 0, 11, 32, 21)
     if object_name == "inventory_purple_item":
         return HsvFilter(126, 153, 0, 255, 255, 255, 0, 0, 0, 0)
-    if object_name == "store_buttons":
-        return HsvFilter(0, 0, 89, 43, 58, 240, 7, 1, 0, 0)
+    if object_name == "button_repair":
+        return None, [208, 600]
 
     # These are all To be done later
     if object_name == "event_card_trade":
@@ -87,6 +91,25 @@ def grab_preset_filter(object_name=None, **kwargs) -> typing.Tuple[HsvFilter, li
             return HsvFilter(0, 0, 0, 255, 255, 255, 0, 0, 0, 0), [10, 145, 1084, 684]
         return HsvFilter(0, 0, 0, 255, 255, 255, 0, 0, 0, 0), [10, 145, 1084, 684]
     if object_name == "cards":
-        return HsvFilter(0, 0, 0, 255, 255, 255, 0, 0, 0, 0)
+        return HsvFilter(0, 0, 0, 255, 255, 255, 0, 0, 0, 0), [735, 32, 1085, 100]
     if object_name == "enemy_arrow":
         return HsvFilter(0, 0, 0, 255, 255, 255, 0, 0, 0, 0), [10, 145, 1084, 684]
+    # Buttons for clicking, known positions
+    if object_name == "button_explore_again":
+        return None, []
+    if object_name == "button_choose_map":
+        return None, []
+    if object_name == "button_open_store":
+        return None, []
+    if object_name == "button_go_town":
+        return None, []
+    if object_name == "button_inv_equipment":
+        return None, []
+    if object_name == "button_inv_consume":
+        return None, []
+    if object_name == "button_inv_other":
+        return None, []
+    if object_name == "button_repair_confirm":
+        return None, []
+    if object_name == "inv_grid_location":
+        return None, [533+44*kwargs.get("col"), 277+44*kwargs.get("row")]

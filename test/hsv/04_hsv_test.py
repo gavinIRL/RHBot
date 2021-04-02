e7ee13b4566115552e236ef52f95ef6de9b0fc3a
Gavin <gavin.feeney01@gmail.com>
2021-04-02 07:32:26 +0100

    Added gui for choosing best hsv filter for #8

---
+++ test/hsv/04_hsv_test.py
@@ -0,0 +1,49 @@
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+from windowcapture import WindowCapture
+from vision import Vision
+from hsvfilter import HsvFilter
+
+# Change the working directory to the folder this script is in.
+# Doing this because I'll be putting the files from each video in their own folder on GitHub
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+WindowCapture.list_window_names()
+# initialize the WindowCapture class
+wincap = WindowCapture('Document15 - Word')
+
+# initialize the Vision class
+vision_limestone = Vision('player_nodir2.jpg')
+# initialize the trackbar window
+vision_limestone.init_control_gui()
+
+# limestone HSV filter
+hsv_filter = HsvFilter(0, 0, 0, 255, 255, 255, 0, 0, 0, 0)
+
+loop_time = time()
+while(True):
+
+    # get an updated image of the game
+    screenshot = wincap.get_screenshot()
+    # pre-process the image
+    output_image = vision_limestone.apply_hsv_filter(screenshot)
+    # do object detection
+    #rectangles = vision_limestone.find(screenshot, 0.5)
+    # draw the detection results onto the original image
+    #output_image = vision_limestone.draw_rectangles(screenshot, rectangles)
+    # display the processed image
+    cv.imshow('Matches', output_image)
+
+    # debug the loop rate
+    print('FPS {}'.format(1 / (time() - loop_time)))
+    loop_time = time()
+
+    # press 'q' with the output window focused to exit.
+    # waits 1 ms every loop to process key presses
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print('Done.')

734a2cf300af7cc0cd7e5ee5f4131662ff1b6d46
Gavin <gavin.feeney01@gmail.com>
2021-04-03 18:34:08 +0100

    Closes #3 - indungeon and otherplayer detection

--- test/hsv/06_detect_test_response.py
+++ test/hsv/06_detect_test_response.py
@@ -11,47 +11,55 @@ from hsvfilter import HsvFilter, grab_object_preset
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # The next block of code is for detecting the object in question
-filter, custom_rect = grab_object_preset(
+object_filter, object_custom_rect = grab_object_preset(
     object_name="other_player_map_loc")
 # WindowCapture.list_window_names()
 # This is only for testing and fixing the 150% screen scaling I have
-custom_rect = list(map(lambda x: int(x*1.5), custom_rect))
+object_custom_rect = list(map(lambda x: int(x*1.5), object_custom_rect))
 # initialize the WindowCapture class for object detection
-wincap_object = WindowCapture(custom_rect=custom_rect)
+object_wincap = WindowCapture(custom_rect=object_custom_rect)
 # initialize the Vision class
-vision_object = Vision('otherplayer.jpg')
+object_vision = Vision('otherplayer.jpg')
 # initialize the trackbar window
-# vision_object.init_control_gui()
+# object_vision.init_control_gui()
 
 
 # This block of code is for detecting if in a dungeon or not
+dunchk_filter, dunchk_custom_rect = grab_object_preset(
+    object_name="dungeon_check")
+# This is only for testing and fixing the 150% screen scaling I have
+dunchk_custom_rect = list(map(lambda x: int(x*1.5), dunchk_custom_rect))
+dunchk_wincap = WindowCapture(custom_rect=dunchk_custom_rect)
+dunchk_vision = Vision('dunchk.jpg')
 
 
 loop_time = time()
 while(True):
 
-    # get an updated image of the game
-    screenshot = wincap_object.get_screenshot()
+    # get an updated image of the game at specified area
+    dunchk_screenshot = dunchk_wincap.get_screenshot()
+    # pre-process the image to help with detection
+    dunchk_output_image = dunchk_vision.apply_hsv_filter(
+        dunchk_screenshot, dunchk_filter)
+    # do object detection, this time grab the points
+    dunchk_rectangles = dunchk_vision.find(
+        dunchk_output_image, threshold=0.27, epsilon=0.5)
 
-    # pre-process the image to detect if currently in dungeon
-
-    # then check if currently in a dungeon
-    status_in_dungeon = True
-
-    if status_in_dungeon:
+    # then if currently in a dungeon search for object
+    if len(dunchk_rectangles) == 1:
+        # get an updated image of the game at map loc
+        screenshot = object_wincap.get_screenshot()
         # then try to detect the other player
-        output_image = vision_object.apply_hsv_filter(screenshot, filter)
+        output_image = object_vision.apply_hsv_filter(
+            screenshot, object_filter)
         # filter_image = output_image.copy()
         # do object detection, this time grab the points
-        rectangles = vision_object.find(
+        rectangles = object_vision.find(
             output_image, threshold=0.41, epsilon=0.5)
         # draw the detection results onto the original image
-        points = vision_object.get_click_points(rectangles)
-        output_image = vision_object.draw_crosshairs(screenshot, points)
-        # display the processed image
-        cv.imshow('Matches', output_image)
-        # cv.imshow('Filtered', filter_image)
+        points = object_vision.get_click_points(rectangles)
         if len(points) == 1:
+            output_image = object_vision.draw_crosshairs(screenshot, points)
             # If there is only one value found
             # i.e. no false positives and players are not on top of each other
             # Then figure out keypresses required to move towards other player
@@ -62,8 +70,12 @@ while(True):
         else:
             # Clear all keypresses
             print("Can't detect other player, stopping movement")
+        # display the processed image
+        cv.imshow('Matches', output_image)
+        # cv.imshow('Filtered', filter_image)
     else:
         print("Not in dungeon, sleeping to slow refresh rate")
+        sleep(0.25)
 
     # debug the loop rate
     # print('FPS {}'.format(1 / (time() - loop_time)))

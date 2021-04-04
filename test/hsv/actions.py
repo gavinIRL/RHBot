b3d959a86923f7f0c2a449a9d699d23f96afe811
Gavin <gavin.feeney01@gmail.com>
2021-04-04 10:05:53 +0100

    Implementing initial relative movement handling

--- test/hsv/actions.py
+++ test/hsv/actions.py
@@ -1,10 +1,25 @@
+import pyautogui as pag
+from time import sleep, time
+
 
 class Actions():
     def __init__(self) -> None:
-        pass
+        self.pressed_keys = []
+        self.start_time = time()
 
     def move_direction(self, relx, rely):
-        pass
+        if relx > 0:
+            # Check if left key held down
+            if "left" in self.pressed_keys:
+                self.pressed_keys.remove("left")
+            # Check that not already being held down
+            if "right" not in self.pressed_keys:
+                self.pressed_keys.append("right")
+                # Hold the right key down
+                pag.keyDown("right")
 
     def click(self, x, y):
         pass
+
+    def elapsed_time(self):
+        return time()-self.start_time

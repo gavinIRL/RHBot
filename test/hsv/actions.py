41dfd654b986395e11c23ca639a08291970f0dbd
Gavin <gavin.feeney01@gmail.com>
2021-04-04 10:12:04 +0100

    Add remaining handling for x direction

--- test/hsv/actions.py
+++ test/hsv/actions.py
@@ -10,17 +10,43 @@ class Actions():
 
     def move_direction(self, relx, rely):
         if relx > 0:
-            # Check if left key held down
+            # Check if opposite key held down
             if "left" in self.pressed_keys:
                 self.pressed_keys.remove("left")
+                if not self.test_mode:
+                    pag.keyUp("left")
             # Check that not already being held down
             if "right" not in self.pressed_keys:
                 self.pressed_keys.append("right")
-                # Hold the right key down
+                # Hold the key down
                 if not self.test_mode:
                     pag.keyDown("right")
                 else:
                     print("Pressing right key")
+        elif relx < 0:
+            # Check if opposite key held down
+            if "right" in self.pressed_keys:
+                self.pressed_keys.remove("right")
+                if not self.test_mode:
+                    pag.keyUp("right")
+            # Check that not already being held down
+            if "left" not in self.pressed_keys:
+                self.pressed_keys.append("left")
+                # Hold the key down
+                if not self.test_mode:
+                    pag.keyDown("left")
+                else:
+                    print("Pressing left key")
+        else:
+            # Handling for case where = 0, need to remove both keys
+            if "right" in self.pressed_keys:
+                self.pressed_keys.remove("right")
+                if not self.test_mode:
+                    pag.keyUp("right")
+            if "left" in self.pressed_keys:
+                self.pressed_keys.remove("left")
+                if not self.test_mode:
+                    pag.keyUp("left")
 
     def click(self, x, y):
         pass

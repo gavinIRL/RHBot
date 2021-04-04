2e2020d6583189e57aa4698db47563e77d079e80
Gavin <gavin.feeney01@gmail.com>
2021-04-04 15:17:31 +0100

    Further testing of movement inputs

--- test/hsv/09_test_direct_input.py
+++ test/hsv/09_test_direct_input.py
@@ -4,7 +4,23 @@ import pyautogui
 import pydirectinput
 import time
 
-time.sleep(4)
-pydirectinput.keyDown('w')
-time.sleep(1)
-pydirectinput.keyUp('w')
+
+def test_move():
+    time.sleep(4)
+    pydirectinput.keyDown('left')
+    time.sleep(1)
+    pydirectinput.keyUp('left')
+
+
+def loop_move():
+    time.sleep(3)
+    count = 0
+    pydirectinput.keyDown("left")
+    while count < 5:
+        time.sleep(0.1)
+        count += 1
+    else:
+        pydirectinput.keyUp("left")
+
+
+loop_move()

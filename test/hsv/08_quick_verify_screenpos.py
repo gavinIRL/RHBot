ed1a206c79428042d57a0de6c6b30e86bd84eaea
Gavin <gavin.feeney01@gmail.com>
2021-04-04 11:09:54 +0100

    Add code to verify screenpos of capture box

--- test/hsv/08_quick_verify_screenpos.py
+++ test/hsv/08_quick_verify_screenpos.py
@@ -1,2 +1,24 @@
 # This file is just for quickly verifying the position of the grab areas in game
 # I have made it standalone for simplicity of setup, no filters being used, etc,
+import cv2 as cv
+import numpy as np
+import os
+from time import time
+from windowcapture import WindowCapture
+from hsvfilter import grab_object_preset
+
+filter, custom_rect = grab_object_preset(
+    object_name="player_map_loc")
+
+wincap = WindowCapture("Rusty Hearts: Revolution - Reborn ", custom_rect)
+loop_time = time()
+while(True):
+    # get an updated image of the game
+    screenshot = wincap.get_screenshot()
+    cv.imshow('Viewbox', screenshot)
+
+    if cv.waitKey(1) == ord('q'):
+        cv.destroyAllWindows()
+        break
+
+print('Done.')

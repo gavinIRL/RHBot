43bdf43dab51c7b08002348add6f19b84d9e4d10
Gavin <gavin.feeney01@gmail.com>
2021-04-04 10:16:06 +0100

    Add remaining directional movement handling

--- test/hsv/actions.py
+++ test/hsv/actions.py
@@ -9,6 +9,8 @@ class Actions():
         self.test_mode = test_mode
 
     def move_direction(self, relx, rely):
+        # Handling for x-dir first
+        # Making this verbose rather than golf for clarity
         if relx > 0:
             # Check if opposite key held down
             if "left" in self.pressed_keys:
@@ -47,6 +49,46 @@ class Actions():
                 self.pressed_keys.remove("left")
                 if not self.test_mode:
                     pag.keyUp("left")
+
+        # Handling for y-dir next
+        if rely > 0:
+            # Check if opposite key held down
+            if "down" in self.pressed_keys:
+                self.pressed_keys.remove("down")
+                if not self.test_mode:
+                    pag.keyUp("down")
+            # Check that not already being held down
+            if "up" not in self.pressed_keys:
+                self.pressed_keys.append("up")
+                # Hold the key down
+                if not self.test_mode:
+                    pag.keyDown("up")
+                else:
+                    print("Pressing up key")
+        elif rely < 0:
+            # Check if opposite key held down
+            if "up" in self.pressed_keys:
+                self.pressed_keys.remove("up")
+                if not self.test_mode:
+                    pag.keyUp("up")
+            # Check that not already being held down
+            if "down" not in self.pressed_keys:
+                self.pressed_keys.append("down")
+                # Hold the key down
+                if not self.test_mode:
+                    pag.keyDown("down")
+                else:
+                    print("Pressing down key")
+        else:
+            # Handling for case where = 0, need to remove both keys
+            if "up" in self.pressed_keys:
+                self.pressed_keys.remove("up")
+                if not self.test_mode:
+                    pag.keyUp("up")
+            if "down" in self.pressed_keys:
+                self.pressed_keys.remove("down")
+                if not self.test_mode:
+                    pag.keyUp("down")
 
     def click(self, x, y):
         pass

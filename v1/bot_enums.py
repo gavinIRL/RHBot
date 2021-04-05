fd1e903ba5eebaf33d4c85f22b27048f9fb7254c
Gavin <gavin.feeney01@gmail.com>
2021-04-05 11:34:25 +0100

    Remove existing placeholder files for v1 (#5, #11)

--- v1/bot_enums.py
+++
@@ -1,8 +0,0 @@
-# This will be the file that holds all of the enum states
-class BotState:
-    STARTUP = 0
-    MOVEMENT = 1
-    COMBAT_SINGLE = 2
-    COMBAT_MULTI = 3
-    LOOT = 4
-    LEVEL_END = 5

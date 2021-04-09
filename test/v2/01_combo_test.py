c9384b336d6830d12b5e738c2b30c0412a0814ae
Gavin <gavin.feeney01@gmail.com>
2021-04-09 19:36:48 +0100

    Added attempt 3 to combo test, threading fix

--- test/v2/01_combo_test.py
+++ test/v2/01_combo_test.py
@@ -3,7 +3,8 @@ import numpy as np
 import os
 from time import time, sleep
 import pydirectinput
-from pynput.keyboard import Key, Listener, KeyCode
+from pynput.keyboard import Key, Listener, KeyCode, Controller
+import threading
 
 
 def basic_combo():
@@ -20,7 +21,7 @@ def basic_combo():
     sleep(0.1)
     pydirectinput.keyUp("x")
 
-
+# Attempt 1
 # while True:
 #     sleep(0.5)
 #     if cv.waitKey(1) == ord('q'):
@@ -28,24 +29,54 @@ def basic_combo():
 #     if cv.waitKey(1) == ord('e'):
 #         basic_combo()
 
+# Attempt 2, fixing problem of not having a window
+# def on_press(key):
+#     pass
+#     # print('{0} pressed'.format(
+#     #     key))
+
+
+# def on_release(key):
+#     # print('{0} release'.format(
+#     #     key))
+#     if key == KeyCode(char='e'):
+#         basic_combo()
+#     if key == Key.esc:
+#         # Stop listener
+#         return False
+
+
+# def start():
+#     # Collect events until released
+#     with Listener(
+#             on_press=on_press,
+#             on_release=on_release) as listener:
+#         listener.join()
+
+#     print("Got to here")
+# start()
+
+# Attempt 3, fixing problem of not being threaded
+listener = None
+keyPressed = None
+
+
 def on_press(key):
-    pass
-    # print('{0} pressed'.format(
-    #     key))
-
-
-def on_release(key):
-    # print('{0} release'.format(
-    #     key))
-    if key == KeyCode(char='e'):
-        basic_combo()
     if key == Key.esc:
         # Stop listener
         return False
 
 
-# Collect events until released
-with Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
-    listener.join()
+def on_release(key):
+    global keyPressed
+    keyPressed = key.char
+
+
+def CheckWhichKeyIsPressed():
+    global listener
+
+    if listener == None:
+        listener = Listener(on_press=on_press,
+                            on_release=on_release, suppress=True)
+        listener.start()
+    print("Got to here")

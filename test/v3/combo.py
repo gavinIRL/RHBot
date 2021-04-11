cac0e3547d2b56c0c59c9a01a6aa430e8d8136a9
Gavin <gavin.feeney01@gmail.com>
2021-04-11 19:10:39 +0100

    Add skeleton methods, handling for skill unlock

--- test/v3/combo.py
+++ test/v3/combo.py
@@ -3,10 +3,17 @@
 # And hold the basic information of cooldown times, etc.
 
 class WeaponBag():
-    def __init__(self) -> None:
-        pass
+    # The preferred layout of skills for this class is as follows:
+    # a =
+    # s =
+    # d =
+    # f =
+    # g =
+    def __init__(self, level, focused) -> None:
+        self.level = level
+        self.focused = focused
 
-    def grab_base_cooldowns():
+    def grab_base_cooldowns(self):
         cooldowns = {"a": 5}
         cooldowns["s"] = 5
         cooldowns["d"] = 5
@@ -14,18 +21,25 @@ class WeaponBag():
         cooldowns["g"] = 5
         cooldowns["h"] = 5
 
-        cooldowns["sa"] = 5
-        cooldowns["ss"] = 5
-        cooldowns["sd"] = 5
-        cooldowns["sf"] = 5
-        cooldowns["sg"] = 5
-        cooldowns["sh"] = 5
+        cooldowns["s+a"] = 5
+        cooldowns["s+s"] = 5
+        cooldowns["s+d"] = 5
+        cooldowns["s+f"] = 5
+        cooldowns["s+g"] = 5
+        cooldowns["s+h"] = 5
 
         cooldowns["f1"] = 5
         cooldowns["f2"] = 5
         cooldowns["f3"] = 5
         cooldowns["f4"] = 5
+        # Need to adjust based on level and if focused or not
         return cooldowns
 
-    def grab_preferred_combo(self):
+    def grab_preferred_combo(current_cds):
+        # For a given set of conditions this will choose
+        # the preferred attack combination
+        pass
+
+    def grab_zoneprep(current_cds):
+        # This will
         pass

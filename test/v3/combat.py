a4557a05f7e7ce707d3c26b1cdc20b3ba8f26fe2
Gavin <gavin.feeney01@gmail.com>
2021-04-11 18:52:51 +0100

    Add basic init class vars

--- test/v3/combat.py
+++ test/v3/combat.py
@@ -3,8 +3,32 @@
 # In particular the relevant weapon in the combo file
 
 class Combat():
-    def __init__(self, weapon, controller) -> None:
-        pass
+    def __init__(self, enabled, weapon="WB") -> None:
+        # Variable to turn combat on/off from main loop
+        self.enabled = enabled
+        # Variable to determine which combos to use
+        self.weapon = weapon
+
+        # Variables for keeping track of which skills are on cooldown
+        # Basic asdfgh skills up first
+        self.cd_a = 0
+        self.cd_s = 0
+        self.cd_d = 0
+        self.cd_f = 0
+        self.cd_g = 0
+        self.cd_h = 0
+        # Then the shift + asdfgh skills
+        self.cd_sa = 0
+        self.cd_ss = 0
+        self.cd_sd = 0
+        self.cd_sf = 0
+        self.cd_sg = 0
+        self.cd_sh = 0
+        # Then the buff skills
+        self.cd_f1 = 0
+        self.cd_f2 = 0
+        self.cd_f3 = 0
+        self.cd_f4 = 0
 
     def update_cooldowns(self):
         pass
@@ -18,5 +42,7 @@ class Combat():
     def stop(self):
         pass
 
-    def update(self):
+    def update_target(self):
+        # This is for pointing character in correct direction
+        # Want to always be pointed towards the bulk of the enemies
         pass

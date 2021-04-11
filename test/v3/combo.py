7b7043e42c16d698d51fb219faa7089e05f53dad
Gavin <gavin.feeney01@gmail.com>
2021-04-11 19:01:51 +0100

    Added skeleton of combo file for WB

---
+++ test/v3/combo.py
@@ -0,0 +1,31 @@
+# This file will hold the weapon-specific information
+# And will include the preferred combinations of attacks
+# And hold the basic information of cooldown times, etc.
+
+class WeaponBag():
+    def __init__(self) -> None:
+        pass
+
+    def grab_base_cooldowns():
+        cooldowns = {"a": 5}
+        cooldowns["s"] = 5
+        cooldowns["d"] = 5
+        cooldowns["f"] = 5
+        cooldowns["g"] = 5
+        cooldowns["h"] = 5
+
+        cooldowns["sa"] = 5
+        cooldowns["ss"] = 5
+        cooldowns["sd"] = 5
+        cooldowns["sf"] = 5
+        cooldowns["sg"] = 5
+        cooldowns["sh"] = 5
+
+        cooldowns["f1"] = 5
+        cooldowns["f2"] = 5
+        cooldowns["f3"] = 5
+        cooldowns["f4"] = 5
+        return cooldowns
+
+    def grab_preferred_combo(self):
+        pass

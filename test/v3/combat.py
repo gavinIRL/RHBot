84534f4b4b83d0e5742427fac45a5a0131444d25
Gavin <gavin.feeney01@gmail.com>
2021-04-11 18:35:28 +0100

    Initial commit towards #31

---
+++ test/v3/combat.py
@@ -0,0 +1,22 @@
+# This will be the file which handles the combat mode
+# The keypresses themselves will be drawn from the combo file
+# In particular the relevant weapon in the combo file
+
+class Combat():
+    def __init__(self, weapon, controller) -> None:
+        pass
+
+    def update_cooldowns(self):
+        pass
+
+    def start(self):
+        pass
+
+    def run(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def update(self):
+        pass

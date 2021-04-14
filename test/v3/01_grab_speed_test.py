80b99d3bb4d2ace101674deb8d73f49c1706a21a
Gavin <gavin.feeney01@gmail.com>
2021-04-14 22:33:07 +0100

    Final speed test setup prior to results

--- test/v3/01_grab_speed_test.py
+++ test/v3/01_grab_speed_test.py
@@ -13,13 +13,14 @@ from hsvfilter import HsvFilter, grab_object_preset
 # Doing this because I'll be putting the files from each video in their own folder on GitHub
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-filter, custom_rect = grab_object_preset("enemy_map_loc")
+filter, custom_rect = grab_object_preset("loot_far")
 WindowCapture.list_window_names()
 # initialize the WindowCapture class
-wincap = WindowCapture("Rusty Hearts: Revolution - Reborn ", custom_rect)
+# wincap = WindowCapture("Rusty Hearts: Revolution - Reborn ")
+wincap2 = WindowCapture("Rusty Hearts: Revolution - Reborn ", custom_rect)
 
 # initialize the Vision class
-vision_limestone = Vision('enemy67.jpg')
+vision_limestone = Vision('dunchk_67.jpg')
 # initialize the trackbar window
 vision_limestone.init_control_gui()
 
@@ -27,13 +28,21 @@ loop_time = time()
 while(True):
 
     # get an updated image of the game
-    screenshot = wincap.get_screenshot()
-    # screenshot2 = wincap.get_screenshot()
+    # screenshot = wincap.get_screenshot()
+    screenshot2 = wincap2.get_screenshot()
+    # screenshot3 = wincap2.get_screenshot()
     # pre-process the image
-    #output_image = vision_limestone.apply_hsv_filter(screenshot)
-    #output_image2 = vision_limestone.apply_hsv_filter(screenshot2)
+    # output_image = vision_limestone.apply_hsv_filter(screenshot3)
+    output_image2 = vision_limestone.apply_hsv_filter(screenshot2)
+    # rectangles = vision_limestone.find(
+    #     output_image, threshold=0.81, epsilon=0.5)
+    rectangles2 = vision_limestone.find(
+        output_image2, threshold=0.81, epsilon=0.5)
+    # rectangles3 = vision_limestone.find(
+    #     output_image2, threshold=0.81, epsilon=0.5)
+
     # display the processed image
-    cv.imshow('Matches', screenshot)
+    cv.imshow('Matches', screenshot2)
 
     # debug the loop rate
     print('FPS {}'.format(1 / (time() - loop_time)))

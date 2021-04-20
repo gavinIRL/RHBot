a1596f0bea6313b48a9e00ae2b242c9b4d8d39be
Gavin <gavin.feeney01@gmail.com>
2021-04-20 17:11:03 +0100

    Add freemove file for #30

---
+++ test/v3.1/freemove.py
@@ -0,0 +1,10 @@
+# This file will allow the user to freely move the character
+# But once an enemy is detected it will swap to combat mode
+
+class StandaloneFreeMove():
+    def __init__(self) -> None:
+        pass
+
+    def freemove_mainloop(self):
+        while True:
+            pass

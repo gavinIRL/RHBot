f0d034b662863b0d3fe61d6d950801c7d44d027c
Gavin <gavin.feeney01@gmail.com>
2021-04-23 10:22:48 +0100

    Closes #43 - modular v3 bot

---
+++ v3/controller.py
@@ -0,0 +1,92 @@
+# This will be the main file that will alternate between
+# movement and combat modes as needs be
+# It will check every 100ms if a module requires starting
+import time
+import os
+from combat_standalone import StandaloneCombat
+from moveloot_standalone import StandaloneMoveLoot
+from freemove_standalone import StandaloneFreeMove
+from pynput.keyboard import Key, Listener, KeyCode
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+class Controller():
+    def __init__(self, loot=True, combat=True, freemove=False) -> None:
+        self.mode = "movement"
+        self.listener = None
+        self.bot_running = False
+        self.loot_enabled = loot
+        self.combat_enabled = combat
+        self.freemove_enabled = freemove
+        self.movebot = StandaloneMoveLoot(self)
+        self.combatbat = StandaloneCombat(self)
+        self.freemovebot = StandaloneFreeMove(self)
+        self.combat_cooldown = 0
+
+    def start_controller(self):
+        self.start_countdown()
+        self.start_keypress_listener()
+        while self.bot_running:
+            time.sleep(0.01)
+            if self.mode == "movement":
+                if not self.freemove_enabled:
+                    self.movebot.move_mainloop()
+                else:
+                    self.freemovebot.freemove_mainloop()
+            elif self.mode == "combat":
+                if (time.time() - self.combat_cooldown) > 0:
+                    self.combatbat.combat_mainloop()
+            else:
+                print("Error, no mode selected")
+                time.sleep(2)
+
+    def start_keypress_listener(self):
+        if self.listener == None:
+            self.listener = Listener(on_press=self.on_press,
+                                     on_release=self.on_release)
+            self.listener.start()
+
+    def on_press(self, key):
+        if key == KeyCode(char='w'):
+            self.loot_enabled = not self.loot_enabled
+            if self.loot_enabled:
+                print("LOOT ON")
+            else:
+                print("LOOT OFF")
+        if key == KeyCode(char='e'):
+            self.combat_enabled = not self.combat_enabled
+            if self.combat_enabled:
+                print("COMBAT ON")
+            else:
+                print("COMBAT OFF")
+        if key == KeyCode(char='r'):
+            self.freemove_enabled = not self.freemove_enabled
+            if self.freemove_enabled:
+                print("FREEMOVE ON")
+            else:
+                print("FREEMOVE OFF")
+
+    def on_release(self, key):
+        if key == KeyCode(char='q'):
+            self.bot_running = False
+            self.combatbat.running = False
+            # Need to pause for 1 second and then clear all keypresses
+            time.sleep(0.5)
+            self.combatbat.remove_all_keypresses()
+            print("Exiting bot")
+            os._exit(1)
+
+    def start_countdown(self):
+        print("Bot starting in 3 seconds")
+        time.sleep(1)
+        print("Bot starting in 2 seconds")
+        time.sleep(1)
+        print("Bot starting in 1 seconds")
+        time.sleep(1)
+        self.bot_running = True
+
+
+if __name__ == "__main__":
+    cont = Controller(freemove=True)
+    cont.start_controller()
---
+++ v3/freemove_standalone.py
@@ -0,0 +1,64 @@
+# This file will allow the user to freely move the character
+# But once an enemy is detected it will swap to combat mode
+from windowcapture import WindowCapture
+from vision import Vision
+from hsvfilter import grab_object_preset
+import os
+import time
+
+
+class StandaloneFreeMove():
+    def __init__(self, controller) -> None:
+        os.chdir(os.path.dirname(os.path.abspath(__file__)))
+        self.controller = controller
+        self.enemy_detect_frames = 0
+
+        # The next block of code is setup for detecting enemies on minimap
+        self.enemy_filter, enemy_custom_rect = grab_object_preset(
+            object_name="enemy_map_locv3")
+        self.enemy_wincap = WindowCapture(
+            "Rusty Hearts: Revolution - Reborn ", enemy_custom_rect)
+        self.enemy_vision = Vision('enemy67.jpg')
+
+    def freemove_mainloop(self):
+        loop_time = time.time()
+        while self.controller.bot_running:
+            # Will attempt to detect enemies, if so will enter combat mode
+            # Only exception would be if combat is disabled
+            # In which case the bot will just sleep
+            if self.controller.combat_enabled:
+                if self.perform_enemy_check():
+                    if (time.time() - self.controller.combat_cooldown) > 0:
+                        self.controller.mode = "combat"
+                        break
+            else:
+                time.sleep(0.25)
+            # If loops are over 100fps, slow to 67fps
+            if 100*(time.time() - loop_time) < 1:
+                # Minimum sleep time is roughly 15ms regardless
+                time.sleep(0.001)
+            loop_time = time.time()
+            if not self.controller.freemove_enabled:
+                break
+
+    def perform_enemy_check(self):
+        if self.check_for_enemies():
+            self.enemy_detect_frames += 1
+            if self.enemy_detect_frames >= 2:
+                return True
+        else:
+            self.enemy_detect_frames = 0
+        return False
+
+    def check_for_enemies(self):
+        enemy_screenshot = self.enemy_wincap.get_screenshot()
+        # pre-process the image to help with detection
+        enemy_output_image = self.enemy_vision.apply_hsv_filter(
+            enemy_screenshot, self.enemy_filter)
+        # do object detection, this time grab points
+        enemy_rectangles = self.enemy_vision.find(
+            enemy_output_image, threshold=0.61, epsilon=0.5)
+        # then return answer to whether enemies are detected
+        if len(enemy_rectangles) >= 1:
+            return True
+        return False

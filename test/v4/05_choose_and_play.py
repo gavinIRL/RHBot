7fc445c0e52d4d543c29205de51ecfb9215453c8
Gavin <gavin.feeney01@gmail.com>
2021-04-25 11:18:22 +0100

    Initial commit towards #51

---
+++ test/v4/05_choose_and_play.py
@@ -0,0 +1,4 @@
+# This file will allow the user to quickly choose
+# an action-sequence to play and then carry out the action sequence
+# It will wait in the background until a certain key is pressed
+# and then it will perform another action

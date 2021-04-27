4de34c5e76a9089886132612f117545c7ceebc81
Gavin <gavin.feeney01@gmail.com>
2021-04-27 18:19:08 +0100

    Added working mouse listen test file #54

--- test/v4/07_mouse_listen_test.py
+++ test/v4/07_mouse_listen_test.py
@@ -20,7 +20,47 @@ class MouseTest():
         self.mouse_listener.start()
         self.mouse_listener.wait()
 
+    def convert_click_to_ratio(self, truex, truey):
+        # This will grab the current rectangle coords of game window
+        # and then turn the click values into a ratio of positions
+        # versus the game window
+        self.game_wincap.update_window_position(border=False)
+        # Turn the screen pos into window pos
+        relx = truex - self.game_wincap.window_rect[0]
+        rely = truey - self.game_wincap.window_rect[1]
+        # Then convert to a ratio
+        ratx = relx/self.game_wincap.w
+        raty = rely/self.game_wincap.h
+
+        return ratx, raty
+
+    def convert_ratio_to_click(self, ratx, raty):
+        # This will grab the current rectangle coords of game window
+        # and then turn the ratio of positions versus the game window
+        # into true x,y coords
+        self.game_wincap.update_window_position(border=False)
+        # Turn the ratios into relative
+        relx = int(ratx * self.game_wincap.w)
+        rely = int(raty * self.game_wincap.h)
+        # Turn the relative into true
+        truex = relx + self.game_wincap.window_rect[0]
+        truey = rely + self.game_wincap.window_rect[1]
+        return truex, truey
+
+    def on_click(self, x, y, button, pressed):
+        # when pressed is False, that means it's a release event.
+        # let's listen only to mouse click releases
+        if not pressed:
+            # Need to get the ratio compared to window top left
+            # This will allow common usage on other size monitors
+            print("x={}, y={}".format(x, y))
+            xratio, yratio = self.convert_click_to_ratio(x, y)
+            print("xrat={}, yrat={}".format(xratio, yratio))
+            xconv, yconv = self.convert_ratio_to_click(xratio, yratio)
+            print("xconv={}, yconv={}".format(xconv, yconv))
+
     def start(self):
+        self.start_mouse_listener()
         while True:
             time.sleep(1)
 
